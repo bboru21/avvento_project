9af2040eb24fa1cf7437319c0635da1a310d4eb6
Bryan Hadro <bryan.e.hadro@gmail.com>
2021-09-15 11:05:23 -0400

    adds email templates, SITE* settings

--- amici/management/commands/send_advent_notification.py
+++ amici/management/commands/send_advent_notification.py
@@ -1,47 +1,61 @@
+import datetime
+
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from amici.models import Friend
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+from amici.models import Friend
 
 
 class Command(BaseCommand):
-    help = 'Sends an Advent Friend noticication e-mail'
+    help = 'Sends an Advent Friend notification e-mail'
 
     def add_arguments(self, parser):
-        parser.add_argument('notification', type=int, choices=(1, 2))
+        parser.add_argument(
+            '--notification',
+            type=int,
+            choices=(1, 2),
+        )
 
     def handle(self, *args, **options):
 
         notification = options['notification']
 
-        subject = 'Advent Friends'
+        year = datetime.date.today().year
+        subject = f'Advent Friends ({year})'
 
         for friend in Friend.objects.filter(active=True):
             if notification == 1:
                 message = f"""
-                    Hi {friend.display_name},
-
                     Advent is just around the corner, and we'll be drawing
                     names in just a few days! Please let me know ASAP if you
                     don't want to participate. Otherwise on November 11th I
                     will email you your secret name!
-
-                    --Bryan
                 """
             elif notification == 2:
                 message = f"""
-                    Hi {friend.display_name},
-
                     Just a reminder that we'll be drawing names tomorrow, so
                     please let me know ASAP if you don't want to participate!
-
-                    --Bryan
                 """
+
+            context = {
+                'notification': notification,
+                'recipient_name': friend.display_name,
+                'content': message,
+                'year': year,
+                'link': f'{settings.SITE_URL}amici/#CHANGEME',
+            }
+
+            message = render_to_string('amici/templates/email.txt', context)
+
+            html_message = render_to_string('amici/templates/email.html', context)
 
             send_mail(
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 subject=subject,
                 message=message,
+                html_message=html_message,
                 recipient_list=(friend.email,),
             )
 
