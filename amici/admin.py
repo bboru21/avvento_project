19d448ca97d6e2af2e61ab9ae7783101f9825bc1
bboru21 <bryan.e.hadro@gmail.com>
2022-11-14 10:20:44 -0500

    adds make_active make_inactive actions to FriendAdmin

--- amici/admin.py
+++ amici/admin.py
@@ -25,7 +25,16 @@ action_export_to_xlsx.short_description = "Export to Excel Spreadsheet"
 class FriendListAdmin(admin.ModelAdmin):
     actions = (action_export_to_xlsx,)
 
+@admin.action(description='Mark selected Friends as active')
+def make_active(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+@admin.action(description='Mark selected Friends as inactive')
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(active=False)
+
 @admin.register(Friend)
 class FriendAdmin(admin.ModelAdmin):
     list_filter = ('active',)
     list_display = ('first_name', 'last_name', 'email',)
+    actions = [make_active, make_inactive]
